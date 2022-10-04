f62eefb7bda0a0cb9de0e3eeaf300a0abd1dcd8a
dilshad360 <dilshad360.dev@gmail.com>
2022-10-04 11:13:30 +0530

    add: about page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Home from './pages/Home';
 import Blog from './pages/Blog';
+import About from './pages/About';
 import { Routes, Route } from 'react-router-dom';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <Routes>
       <Route path='/' element={<Home/>} />
       <Route path='/blog/:id' element={<Blog/>} />
+      <Route path='/about' element={<About/>} />
     </Routes>
   );
 }
---
+++ src/pages/About/index.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+function About() {
+  return (
+    <div>
+      <p>
+        What if there exist an ideal opportunity to hightlight the intangible
+        thoughts linering in your mind?
+      </p>
+      <p>
+        <b>Connect EMEA</b> Provides a golden opportunity to imprint your
+        impressions and showcase it to the world.
+      </p>
+      <ul>
+        <li>Writings of any language are permitted </li>
+        <li>It can be both academic or non-academic style. </li>
+        <li>No word limit. </li>
+        <li>
+          Could be any genre (poems, essays, stories, articles, anecdotes){" "}
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default About;
